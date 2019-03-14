eeb660e704d79d93426b7824ca6b6889554c5f37
vikasvisking007 <vikasvisking007@gmail.com>
2019-03-14 15:51:05 +0530

    setting the project

--- novus/settings.py
+++ novus/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'novus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_LOCATION = 'static'
+
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'staticFiles') ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
