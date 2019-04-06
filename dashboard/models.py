334558be8a5963f3a9033f6603678c2715cb4b7e
vikasvisking007 <vikasvisking007@gmail.com>
2019-04-06 17:58:28 +0530

    change theame

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#Extending default User model   with required fields
+class UserModel(models.Model):
+	name = models.OneToOneField(User, on_delete = models.CASCADE)
+	# image = models.ImageField(upload_to = '/profile')
